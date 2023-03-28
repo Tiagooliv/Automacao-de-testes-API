e67cd8b53f6198ab2b93c5cf2a2063f416377e4a
Tiago de Oliveira Silva <re045690@qintess.com>
2023-03-28 18:00:09 -0300

    Validacoes

--- CursoRestAssured/src/main/java/ola/mundo/OlaMundoTest.java
+++ CursoRestAssured/src/main/java/ola/mundo/OlaMundoTest.java
@@ -12,19 +12,19 @@ public class OlaMundoTest {
 
 	@Test
 	public void TestOlaMundo() {
-		
-		 Response response = RestAssured.request(Method.GET,"https://restapi.wcaquino.me/ola");
-		  
-		  Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
-		  //Validação 1
-		  
-		  //Validações do código de resposta
-		  Assert.assertTrue("O status code deveria ser 200.",
-		  response.statusCode()==200); Assert.assertEquals(2003,
-		  response.statusCode());
-		  
-			ValidatableResponse validacao = response.then(); validacao.statusCode(200);
-		 
-		
+
+		Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
+
+		Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
+		// Validação 1
+
+		// Validações do código de resposta
+		Assert.assertTrue("O status code deveria ser 200.", response.statusCode() == 200);
+
+		Assert.assertEquals(200, response.statusCode());
+
+		ValidatableResponse validacao = response.then();
+		validacao.statusCode(200);
+
 	}
 }
