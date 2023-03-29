25029bca5e69de42eb1e39093bfc1ad61080fbe0
Tiago de Oliveira Silva <re045690@qintess.com>
2023-03-29 12:03:12 -0300

    testerro

--- CursoRestAssured/src/main/java/ola/mundo/OlaMundoTest.java
+++ CursoRestAssured/src/main/java/ola/mundo/OlaMundoTest.java
@@ -24,7 +24,7 @@ public class OlaMundoTest {
 		Assert.assertEquals(200, response.statusCode());
 
 		ValidatableResponse validacao = response.then();
-		validacao.statusCode(200);~~~~~~~~~~
+		validacao.statusCode(200);ooooo
 
 	}
 }
